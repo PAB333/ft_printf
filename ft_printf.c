6995bc2be67a95a8a6c02213a8f2245fedef2e6a
Pierre-Antoine Breissand <pibreiss@z4r4p6.42lyon.fr>
2024-11-20 23:32:19 +0100

    printf incoming

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: pibreiss <pibreiss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 11:50:17 by pibreiss          #+#    #+#             */
-/*   Updated: 2024/11/19 12:17:37 by pibreiss         ###   ########.fr       */
+/*   Updated: 2024/11/20 23:31:46 by pibreiss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,26 @@
 
 int ft_printf(const char *format, ...)
 {
-	size_t	i;
+	int		i;
 	va_list	arg;
 
 	i = -1;
 	va_start(arg, format);
 	while (format[++i])
 	{
-		if (format[i] == '%' && format[i + 1] == 'd')
-			
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == 'd' || format[i + 1] == 'i')
+			if (format[i + 1] == 'u')
+			if (format[i + 1] == 'x' || format[i + 1] == 'X')
+			if (format[i + 1] == 's')
+			if (format[i + 1] == 'c')
+				ft_putchar(va_arg(arg, char));
+			if (format[i + 1] == 'p')
+			if (format[i + 1] == '%')
+				ft_putchar('%');
+		}
+		else
+			ft_putchar(&format[i]);
 	}
 }
--- ft_putchar.c
+++ ft_putchar.c
@@ -1,25 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_putchar.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pibreiss <pibreiss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/19 11:47:08 by pibreiss          #+#    #+#             */
-/*   Updated: 2024/11/20 23:27:41 by pibreiss         ###   ########.fr       */
+/*   Created: 2024/11/20 23:26:08 by pibreiss          #+#    #+#             */
+/*   Updated: 2024/11/20 23:26:55 by pibreiss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#include "ft_printf.h"
 
-# include <unistd.h>
-# include <stdlib.h>
-# include <stdarg.h>
-
-int		ft_printf(const char *format, ...);
-void	ft_putchar(char c);
-void	ft_putstr(char *str);
-void	ft_putnbr(int nb);
-
-#endif
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
